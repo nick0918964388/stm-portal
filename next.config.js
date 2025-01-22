5bf6a295b0d9cce526569d2db816cdd0f260e57d
nickyin <nickyin@tw.ibm.com>
2025-01-22 18:40:07 +0800

    aa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@ant-design/charts', 'antd', '@ant-design/plots', '@ant-design/icons', 'rc-util', 'rc-tree'],
+  transpilePackages: [
+    '@ant-design/charts',
+    '@ant-design/plots',
+    '@ant-design/icons',
+    'antd',
+    'rc-util',
+    'rc-tree',
+    'rc-table',
+    'rc-picker',
+    'rc-pagination',
+    'rc-dialog',
+    'rc-select',
+    'rc-cascader',
+    'rc-checkbox',
+    'rc-dropdown',
+    'rc-menu',
+    'rc-input',
+    'rc-input-number',
+    'rc-motion',
+    'rc-notification',
+    'rc-tooltip',
+    'rc-tabs',
+    'rc-tree-select',
+    '@babel/runtime'
+  ],
   webpack: (config) => {
     config.resolve.extensionAlias = {
       '.js': ['.js', '.ts', '.tsx'],
